817be038fb2c0d3a9f451ed0d02172b1cbe1c63a
Abhishek Agarwal <iamabhishekagarwal@gmail.com>
2024-09-28 10:06:34 +0530

    Setting up the folders

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+const port = 5172;
+
+
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
