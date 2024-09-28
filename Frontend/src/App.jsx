7b1ed9f927cd21399dd975cef2f342c9dc22a623
adityamishra139 <aditya2vibha@gmail.com>
2024-09-28 15:42:32 +0530

    bonus

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Homepage from './pages/Homepage'
 
 import Wardrobe from './pages/Wardrobe'
+import CommunityExchange from './pages/CommunityExchange'
+
 
 function App() {
    return (
     <>
-      <Homepage></Homepage>
+      {/* <Homepage></Homepage> */}
       {/* <Wardrobe></Wardrobe> */}
+      <CommunityExchange></CommunityExchange>
     </>
   )
 }
