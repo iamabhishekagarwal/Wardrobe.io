9b73fcb5b620a8503020e9c546eb7175c22d4dce
Abhishek Agarwal <iamabhishekagarwal@gmail.com>
2024-09-28 11:14:05 +0530

    Added Authentication

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 // src/components/Navbar.jsx
 import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const Navbar = () => {
+  const { loginWithRedirect ,isAuthenticated,logout } = useAuth0();
+
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
@@ -12,9 +16,12 @@ const Navbar = () => {
           <li className="hover:text-gray-400"><a href="#">Pricing</a></li>
           <li className="hover:text-gray-400"><a href="#">Contact</a></li>
         </ul>
-        <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md">
+        {isAuthenticated?<><button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md">
+          Logout
+        </button></>:<><button onClick={() => loginWithRedirect()} className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md">
           Login
-        </button>
+        </button></>}
+        
       </div>
     </nav>
   );
